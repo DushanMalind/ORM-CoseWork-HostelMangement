09963c9bfed73cf219664f8650b5634b5b7916d3
DushanMalind <dushanmalinda010@gmail.com>
2023-03-27 17:04:59 +0530

    AppInitializer ok

--- Hostel_Management/src/lk/ijse/hostelManagement/AppInitializer.java
+++ Hostel_Management/src/lk/ijse/hostelManagement/AppInitializer.java
@@ -1,4 +1,23 @@
 package lk.ijse.hostelManagement;
 
-public class AppInitializer {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root= FXMLLoader.load(this.getClass().getResource("view/main_from.fxml"));
+        Scene mainScene=new Scene(root);
+        primaryStage.setScene(mainScene);
+        primaryStage.setTitle("Hostel Management");
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
 }
--- Hostel_Management/src/lk/ijse/hostelManagement/controller/MainFromController.java
+++ Hostel_Management/src/lk/ijse/hostelManagement/controller/MainFromController.java
@@ -1,4 +1,40 @@
 package lk.ijse.hostelManagement.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class MainFromController {
+
+    @FXML
+    private ImageView imgReservation;
+
+    @FXML
+    private ImageView imgRoom;
+
+    @FXML
+    private ImageView imgStudent;
+
+    @FXML
+    private Label lblDescription;
+
+    @FXML
+    private Label lblHead;
+
+    @FXML
+    void navigate(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseEnterAnimation(MouseEvent event) {
+
+    }
+
+    @FXML
+    void playMouseExitAnimation(MouseEvent event) {
+
+    }
+
 }
